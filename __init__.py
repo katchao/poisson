8a01276413b0b09297445c6a04de2158e03ce5ba
Katherine Chao <kchao1994@gmail.com>
2015-08-21 00:17:14 -0700

    test

--- __init__.py
+++ __init__.py
@@ -53,8 +53,9 @@ def index():
 def submit_source():
 	if request.method == 'POST' and 'source' in request.files:
 		source = request.files['source']
-		db['source_filename'] = construct_random_filename(secure_filename(source.filename))
 		print "DB: ", db
+		db['source_filename'] = construct_random_filename(secure_filename(source.filename))
+		
 		print "save path: ", os.path.join(app.config['IMAGES_FOLDER'], db['source_filename'])
 		source.save(os.path.join(app.config['IMAGES_FOLDER'], db['source_filename']))
 
